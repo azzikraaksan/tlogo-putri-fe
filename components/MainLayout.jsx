217ccce2158fc9e10727ceed8a13a8803ebd093d
azzikraaksan <azzikraaksan@gmail.com>
2025-04-11 14:04:29 +0700

    repo awal

---
+++ components/MainLayout.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+// const MainLayout = ({children}) => {
+//   return (
+//     <div>
+//       <h1>Jeep Tlogo Putri</h1>
+//       <hr />
+//       <main>{children}</main>
+//     </div>
+//   )
+// }
+
+// export default MainLayout
+// app/components/MainLayout.jsx
+const MainLayout = ({ children }) => {
+    return (
+      <div className="flex">
+        {/* Sidebar */}
+        <aside className="w-64 h-screen bg-gray-800 text-white p-4">
+          <h2 className="text-xl font-bold mb-4">LOGO</h2><br />
+          <ul>
+            <li><a href="/">Dashboard</a></li><br /><br />
+            <li><a href="/profile">Penjadwalan</a></li><br />
+            <li><a href="/settings">Daftar Jeep</a></li>
+          </ul>
+        </aside>
+  
+        {/* Konten utama */}
+        <main className="flex-1 p-6 bg-gray-100">
+          {children}
+        </main>
+      </div>
+    );
+  };
+  
+  export default MainLayout;
+  
---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata = {
+  title: "Operational Jeep Tlogo Putri",
+  description: "Website untuk mengelola operasional Komunitas Jeep Tlogo Putri",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/* <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      > */}
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
